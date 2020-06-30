62ad4c33d1a6d595ee46f9959f3c0f8e02d43dae
guanjian1997 <guanjian1997@gmail.com>
2020-06-30 16:52:27 +0900

    project init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 统一在不同浏览器上css的样式
+import './assets/styles/reset.css'
+// 移动端有1像素边框的问题 高清手机一像素边框会在高清手机上显示两个像素
+import './assets/styles/border.css'
+// 移动端中有300毫秒点击延迟的问题，用fastclick来解决
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+// 这样就正确使用了fastclick解决了移动端点击事件300毫秒延迟的问题
+fastClick.attach(document.body)
 /* eslint-disable no-new */
+/* 定义了一个vue的根实例，挂载在id=app的元素上 */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
 })
+/* 整个项目的入口文件 */
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  hello world
+<!-- <div class="home">home</div> -->
+<!-- 做页面跳转在vue中一般不使用a标签，一般用router-link标签 -->
+<!-- <router-link to="/list" class="home">列表页</router-link> -->
+</div>
+</template>
+
+<script>
+/* 导出一个对象 */
+export default {
+/* 组件的名字 */
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
